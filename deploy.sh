d1a843b13eccb380620d88fc0a6cf3379c076c10
Spencer Gibb <spencer@gibb.us>
2015-07-22 11:55:52 -0600

    for oscon 2015 fixes

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,10 @@ cp CNAME public/
 
 # Go To Public folder
 cd public
+
+# hack to fix bad link given at oscon
+cp preso/spring-cloud-oscon-2015/index.html preso/spring-cloud-oscon-2015.html
+
 # Add changes to git.
 git add -A
 
